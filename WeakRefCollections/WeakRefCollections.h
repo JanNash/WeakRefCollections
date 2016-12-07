a286202f39523cd58e887f6030c92f623b326896
Jan Nash <jnash@jnash.de>
2016-12-08 00:39:25 +0100

    Create Xcode project

---
+++ WeakRefCollections/WeakRefCollections.h
@@ -0,0 +1,19 @@
+//
+//  WeakRefCollections.h
+//  WeakRefCollections
+//
+//  Created by Jan Nash on 12/8/16.
+//  Copyright © 2016 JanNash. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeakRefCollections.
+FOUNDATION_EXPORT double WeakRefCollectionsVersionNumber;
+
+//! Project version string for WeakRefCollections.
+FOUNDATION_EXPORT const unsigned char WeakRefCollectionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeakRefCollections/PublicHeader.h>
+
+
