9694a43965350e862a41206b4413b2183b79577f
Jan Nash <jnash@jnash.de>
2016-12-17 21:36:12 +0100

    Start creating WeakRefArray by implementing all Array-inits

--- WeakRefCollections/Sources/Public/Types/WeakRefArray.swift
+++ WeakRefCollections/Sources/Public/Types/WeakRefArray.swift
@@ -7,3 +7,35 @@
 //
 
 import Foundation
+
+
+// MARK: // Public
+// MARK: Class Declaration
+public class WeakRefArray<Element: AnyObject> {
+    // Inits
+    public init() {
+        self._array = []
+    }
+    
+    public init<S : Sequence>(_ s: S) where S.Iterator.Element == Element {
+        self._array = s.map({ WeakWrapper_($0) })
+    }
+    
+    public init(repeating repeatedValue: Element, count: Int) {
+        self._array = Array(repeating: WeakWrapper_(repeatedValue), count: count)
+    }
+    
+    // ExpressibleByArrayLiteral
+    required public init(arrayLiteral elements: Element...) {
+        self._array = elements.map({ WeakWrapper_($0) })
+    }
+    
+    // Private Variable Properties
+    fileprivate var _array: Array<WeakWrapper_<Element>>
+}
+
+
+// MARK: ExpressibleByArrayLiteral
+extension WeakRefArray: ExpressibleByArrayLiteral {
+    
+}
