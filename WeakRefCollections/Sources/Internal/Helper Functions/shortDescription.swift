79d9ed0c2fa234b845f95160de23c2504a11a54a
Jan Nash <jnash@jnash.de>
2017-01-15 21:39:42 +0100

    Move functionality of ShortStringConvertible into functions

---
+++ WeakRefCollections/Sources/Internal/Helper Functions/shortDescription.swift
@@ -0,0 +1,41 @@
+//
+//  shortDescription.swift
+//  WeakRefCollections
+//
+//  Created by Jan Nash on 1/15/17.
+//  Copyright © 2017 JanNash. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: // Internal
+// MARK: Function Declarations
+func shortDescription(of object: AnyObject) -> String {
+    return _shortDescription(of: object)
+}
+
+
+func shortDescription(of object: AnyObject?) -> String {
+    return _shortDescription(of: object)
+}
+
+
+// MARK: // Private
+// MARK: Function Implementations
+private func _shortDescription(of object: AnyObject) -> String {
+    let typeString: String = "\(type(of: object))"
+    let identifierString: String = ObjectIdentifier(object).debugDescription
+    let prefixRemoved: String = identifierString.replacingOccurrences(of: "ObjectIdentifier(", with: "")
+    let identityString: String = prefixRemoved.replacingOccurrences(of: ")", with: "")
+    return "<\(typeString): \(identityString)>"
+}
+
+
+private func _shortDescription(of object: AnyObject?) -> String {
+    if let unwrappedObject: AnyObject = object {
+        return _shortDescription(of: unwrappedObject)
+    } else {
+        return "nil"
+    }
+}
